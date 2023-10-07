12eae55ad63efea2ba6c6d056f506951ba966f2b
Michał Rusak <104869035+michalrusak@users.noreply.github.com>
2023-10-07 20:04:41 +0200

    Add footer and refactor

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,29 @@ import React from "react";
 import Navbar from "./components/navbar/Navbar";
 
 import "./styles/main.scss";
+import Footer from "./components/footer/Footer";
+import { Routes, Route } from "react-router-dom";
+import { RouterEnum } from "./enums/RouterEnum";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import NotFound from "./pages/notFound/NotFound";
+import Register from "./pages/register/Register";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <main className="container">
+        <Routes>
+          <Route path={RouterEnum.home} element={<Home />} />
+          <Route path={RouterEnum.login} element={<Login />} />
+          <Route path={RouterEnum.register} element={<Register />} />
+          <Route path={RouterEnum.notFound} element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
