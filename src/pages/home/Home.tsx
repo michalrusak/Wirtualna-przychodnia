13e00d699aa6f0f2749db6e881c31ea4a539f105
Michał Rusak <104869035+michalrusak@users.noreply.github.com>
2023-10-30 19:36:51 +0100

    Add home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,36 +1,44 @@
-import { useContext, useEffect } from "react";
-import { UserContext } from "../../context/UserContext";
-import authService from "../../services/auth-service";
-import databaseService from "../../services/database-service";
+import { useEffect } from "react";
+import "./Home.scss";
 
 const Home = () => {
   useEffect(() => {
     document.title = "Strona główna | Wirtulna przychodnia";
-    getUser();
-    getDoctors();
   });
 
-  const { isUserLogged, displayName } = useContext(UserContext);
-
-  const getUser = async () => {
-    const user = await authService.getUser();
-    // console.log(user);
-
-    const doctor = await authService.isDoctor();
-    console.log(doctor ? "to jest leakrz" : "zwykły uzyt");
-  };
-
-  const getDoctors = async () => {
-    const drs = await databaseService.getdoctors();
-    console.log(drs);
-  };
-
   return (
-    <div>
-      <p>Home</p>
-      <p>{isUserLogged ? "Zalogowany" : "Brak użytkownika"}</p>
-      <p>{displayName}</p>
-      {/* <p>{ doctor ? "leakrz" : "zwykły"}</p> */}
+    <div className="home">
+      <section className="home__welcome">
+        <h1 className="home__title">Witamy w naszej przychodni!</h1>
+        <p className="home__paragraph">
+          Cieszymy się, że odwiedziłeś naszą przychodnię. Jesteśmy gotowi do
+          świadczenia kompleksowej opieki zdrowotnej dla Ciebie i Twojej
+          rodziny. Nasza misja to zapewnić najwyższą jakość opieki medycznej,
+          dbając o Twoje zdrowie.
+        </p>
+      </section>
+      <section className="home__advantage">
+        <h3 className="home__advantage-title">Doświadczona Kadra Lekarska</h3>
+        <p className="home__description">
+          Nasza przychodnia to zespół doświadczonych i wykwalifikowanych
+          lekarzy, pielęgniarek i specjalistów, którzy zawsze są gotowi, by
+          służyć Ci pomocą.
+        </p>
+      </section>
+      <section className="home__advantage">
+        <p className="home__description">
+          Oferujemy szeroki zakres usług medycznych, począwszy od wizyt
+          rodzinnych, przez diagnostykę, aż po opiekę specjalistyczną.
+        </p>
+        <h3 className="home__advantage-title">Kompleksowa Opieka</h3>
+      </section>
+      <section className="home__advantage">
+        <h3 className="home__advantage-title">Nowoczesna Diagnostyka</h3>
+        <p className="home__description">
+          Nasza przychodnia jest wyposażona w najnowocześniejszy sprzęt
+          diagnostyczny, co pozwala na szybką i dokładną diagnozę.
+        </p>
+      </section>
     </div>
   );
 };
